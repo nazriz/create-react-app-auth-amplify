372636126b55cab7c5b95f5357ff7cf34173f5bf
MOHAMED ADNAN USOOF <19842078@students.latrobe.edu.au>
2022-05-27 18:07:09 +1000

    Added UI

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import DeleteItem from './Delete_item';
 import AddImageTags from './addImageTags';
 import RemoveImageTags from './removeImageTags'
 import { Auth } from 'aws-amplify';
+import { NavLink, Routes, Route } from 'react-router-dom';
 Amplify.configure(aws_exports);
 
 Auth.currentSession().then(res => {
@@ -26,16 +27,84 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <h1>Welcome to the Object Detection Web Application</h1>
         <AmplifySignOut />
-        <DropzoneItag />
-        <DropzoneImageForImage />
-        <SearchByTag />
-        <AddImageTags />
-        <RemoveImageTags />
-        <DeleteItem />
+        <Navigation />
+        <Main />
       </div>
     );
   }
 }
+
+
+
+const Navigation = () => (
+  <nav>
+    <ul>
+      <li><NavLink exact activeClassName="home" to='/'>Upload Image</NavLink></li>
+      <li><NavLink exact activeClassName="about" to='/imgforimage'>Search Images by Uploading an Image</NavLink></li>
+      <li><NavLink exact activeClassName="current" to='/imgfortag'>Search Images By Tags</NavLink></li>
+      <li><NavLink exact activeClassName="current" to='/addtag'>Add Tags</NavLink></li>
+      <li><NavLink exact activeClassName="current" to='/removeimagetags'>Remove Image Tags</NavLink></li>
+      <li><NavLink exact activeClassName="current" to='/deleteitem'>Delete Items</NavLink></li>
+    </ul>
+  </nav>
+);
+
+const Home = () => (
+  <div className='home'>
+    <p> Upload Image</p>
+    <DropzoneItag />
+  </div>
+);
+
+const ImgForImage = () => (
+  <div className='about'>
+    <p> Search Image for Images</p>
+    <DropzoneImageForImage />
+  </div>
+  );
+
+const ImgForTag = () => (
+  <div className='ImgForTag'>
+    <p> Search Image By Tags</p>
+    <SearchByTag />
+  </div>
+  );
+    
+const AddTag = () => (
+  <div className='AddTag'>
+    <p> Add Tags</p>
+    <AddImageTags />
+  </div>
+);
+
+const RemoveImageTagss = () => (
+  <div className='RemoveImageTags'>
+    <p> Remove Tags</p>
+    <RemoveImageTags />
+  </div>
+);
+
+const DeleteItems = () => (
+  <div className='DeleteItem'>
+    <p> Delete Items</p>
+    <DeleteItem />
+  </div>
+);
+
+const Main = () => (
+  <Routes>
+      <Route path='/' element={<Home/>}></Route>
+      <Route path='/ImgForImage' element={<ImgForImage/>}></Route>
+      <Route path='/ImgForTag' element={<ImgForTag/>}></Route>
+      <Route path='/AddTag' element={<AddTag/>}></Route>
+      <Route path='/RemoveImageTags' element={<RemoveImageTagss/>}></Route>
+      <Route path='/DeleteItem' element={<DeleteItems/>}></Route>
+  </Routes> 
+);
+
+
+
 
 export default withAuthenticator(App);
